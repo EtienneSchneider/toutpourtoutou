194da70dbc2542c67ed83c7b5b557922ae382a90
YannisAd <yannaddi48@gmail.com>
2024-03-22 15:39:31 +0100

    product page

--- client/src/components/product_components/DeliveryProduct.jsx
+++ client/src/components/product_components/DeliveryProduct.jsx
@@ -3,20 +3,22 @@ import "./DeliveryProduct.scss";
 import "./ProductCounter.scss";
 import ButtonPlus from "./ProductCounter";
 
-const DeliveryProduct = ({ title, desc, link, img_url, price }) => {
+const DeliveryProduct = ({ title, desc, link, img, price }) => {
     return (
         <Link to={link} className="DeliveryProduct bg-default">
             <div className="img-container">
-                <img className="img" src={img_url} alt="" />
+                <img className="img" src={img} alt="" />
                 <div className="buttonPlusProduct">
                     <ButtonPlus />
                 </div>
             </div>
 
-            <div className="text-container">
+            <div className="info-container">
+                <div className="text-container"> 
                 <span className="col-black">{title}</span>
-                <p className="col-black">{price}€</p>
+                <p className="col-black">{price}</p>
                 <p className="col-gray">{desc}</p>
+                </div>
             </div>
         </Link>
     );
