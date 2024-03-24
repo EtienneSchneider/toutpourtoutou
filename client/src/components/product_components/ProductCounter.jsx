a935c174442b2bd5149bd241b76c97d57372d2b5
lucas-stallknecht <124690694+lucas-stallknecht@users.noreply.github.com>
2024-03-24 17:21:02 +0100

    singleproduct no backend

--- client/src/components/product_components/ProductCounter.jsx
+++ client/src/components/product_components/ProductCounter.jsx
@@ -1,26 +1,21 @@
 import React, { useState } from "react";
 import "./ProductCounter.scss";
 
-const ProductCounter = ({value, updateCounter}) => {
-
+const ProductCounter = ({ value, updateCounter }) => {
     function increase() {
-        updateCounter(value + 1)
+        updateCounter(value + 1);
     }
 
     function decrease() {
         if (value > 0) {
-            updateCounter(value - 1)
+            updateCounter(value - 1);
         }
     }
 
     return (
         <div className="count-wrapper">
             <button onClick={decrease}>-</button>
-            <span
-                className="count"
-            >
-                {value}
-            </span>
+            <span className="count">{value}</span>
             <button onClick={increase}>+</button>
         </div>
     );
